31a721e432f35856e753437784b8d566db94b9ad
aiharanaoya <nao11aihara@gmail.com>
2023-11-30 18:36:13 +0900

    tailwaindの設定を削除

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      }
-    }
-  },
+  theme: {},
   plugins: []
 };
 
