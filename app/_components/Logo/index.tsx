76635103c9c660b27e541769a72f992a2bcb85fb
aiharanaoya <nao11aihara@gmail.com>
2023-12-01 00:14:48 +0900

    ロゴにフォントを指定

--- app/_components/Logo/index.tsx
+++ app/_components/Logo/index.tsx
@@ -1,8 +1,11 @@
+import { Abril_Fatface } from 'next/font/google';
 import { FC } from 'react';
+
+export const font = Abril_Fatface({ weight: '400', subsets: ['latin'] });
 
 /**
  * ロゴ
  */
 export const Logo: FC = () => {
-  return <span className="text-2xl font-bold text-slate-800">Fashion EC</span>;
+  return <span className={`${font.className} text-3xl text-slate-800`}>Fashion EC</span>;
 };
