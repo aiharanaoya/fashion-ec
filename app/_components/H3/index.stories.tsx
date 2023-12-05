47931ff6590473bc19a254fa185772a31918fb02
aiharanaoya <nao11aihara@gmail.com>
2023-12-05 13:32:02 +0900

    h3コンポーネント作成

---
+++ app/_components/H3/index.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta as _Meta, StoryObj } from '@storybook/react';
+
+import { H3 } from '.';
+
+type Meta = _Meta<typeof H3>;
+type Story = StoryObj<typeof H3>;
+
+export default {
+  component: H3,
+  args: {
+    text: '見出し'
+  }
+} as Meta;
+
+export const Default: Story = {
+  args: {}
+};
