35d9f3fdc2584731297a3d53c158e4973c008ea2
aiharanaoya <nao11aihara@gmail.com>
2023-12-07 23:48:03 +0900

    アイコンボタンコンポーネント作成

---
+++ app/_components/IconButton/index.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps, FC, ReactNode } from 'react';
+
+type Props = {
+  /** children */
+  children: ReactNode;
+  /** component props */
+} & Pick<ComponentProps<'button'>, 'onClick'>;
+
+/**
+ *  アイコンボタン
+ */
+export const IconButton: FC<Props> = ({ children, ...rest }: Props) => {
+  return (
+    <button
+      {...rest}
+      className="
+        rounded-full p-3 duration-300
+        hover:bg-slate-200
+      "
+    >
+      {children}
+    </button>
+  );
+};
