8256ed513207d7a63305740de59cbcb5dd241d9b
aiharanaoya <nao11aihara@gmail.com>
2023-12-12 19:33:31 +0900

    ヘッダーコンポーネントのロジック追加

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import { HeartIcon, ShoppingCartIcon, UserIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 
 import { FormSearch } from '@/_components/FormSearch';
 import { IconButton } from '@/_components/IconButton';
@@ -10,6 +12,34 @@ import { Logo } from '@/_components/Logo';
  * ヘッダー
  */
 export const Header: FC = () => {
+  /** 検索ワード */
+  const [searchValue, setSearchValue] = useState('');
+
+  /** 検索フォーム入力処理 */
+  const changeFormSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  /** 検索処理 */
+  const handleClickSearch = () => {
+    console.log('検索処理');
+  };
+
+  /** お気に入りアイコン押下時処理 */
+  const handleClickFavorite = () => {
+    console.log('お気に入りアイコン押下時処理');
+  };
+
+  /** カートアイコン押下時処理 */
+  const handleClickCart = () => {
+    console.log('カートアイコン押下時処理');
+  };
+
+  /** ユーザーアイコン押下時処理 */
+  const handleClickUser = () => {
+    console.log('ユーザーアイコン押下時処理');
+  };
+
   return (
     <div
       className="
@@ -22,16 +52,20 @@ export const Header: FC = () => {
       </Link>
       <div className="flex items-center gap-x-2">
         <div className="w-80 text-sm">
-          <FormSearch value="" onChangeForm={() => {}} onClickButton={() => {}} />
+          <FormSearch
+            value={searchValue}
+            onChangeForm={changeFormSearch}
+            onClickButton={handleClickSearch}
+          />
         </div>
         <div className="flex items-center gap-x-1">
-          <IconButton onClick={() => {}}>
+          <IconButton onClick={handleClickFavorite}>
             <HeartIcon className="w-6" />
           </IconButton>
-          <IconButton onClick={() => {}}>
+          <IconButton onClick={handleClickCart}>
             <ShoppingCartIcon className="w-6" />
           </IconButton>
-          <IconButton onClick={() => {}}>
+          <IconButton onClick={handleClickUser}>
             <UserIcon className="w-6" />
           </IconButton>
         </div>
