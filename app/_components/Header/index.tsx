c756a5dc96035b4c0a873a59083717a49ffb40ca
aiharanaoya <nao11aihara@gmail.com>
2023-12-12 18:54:31 +0900

    ヘッダーコンポーネント作成

---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,38 @@
+import { HeartIcon, ShoppingCartIcon, UserIcon } from '@heroicons/react/24/outline';
+import { FC } from 'react';
+
+import { FormSearch } from '@/_components/FormSearch';
+import { IconButton } from '@/_components/IconButton';
+import { Logo } from '@/_components/Logo';
+
+/**
+ * ヘッダー
+ */
+export const Header: FC = () => {
+  return (
+    <div
+      className="
+        flex h-full w-full items-center justify-between
+        border-b border-slate-300 px-8 py-2
+      "
+    >
+      <Logo />
+      <div className="flex items-center gap-x-2">
+        <div className="w-80">
+          <FormSearch value="" onChangeForm={() => {}} onClickButton={() => {}} />
+        </div>
+        <div className="flex items-center gap-x-1">
+          <IconButton>
+            <HeartIcon className="w-6" />
+          </IconButton>
+          <IconButton>
+            <ShoppingCartIcon className="w-6" />
+          </IconButton>
+          <IconButton>
+            <UserIcon className="w-6" />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
