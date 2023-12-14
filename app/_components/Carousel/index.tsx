cd996e54a796d6f434d8fe3a8f8b9f30dbc42110
aiharanaoya <nao11aihara@gmail.com>
2023-12-14 17:39:30 +0900

    選択中のボタンを追加

--- app/_components/Carousel/index.tsx
+++ app/_components/Carousel/index.tsx
@@ -2,7 +2,8 @@
 
 import useEmblaCarousel from 'embla-carousel-react';
 import Image from 'next/image';
-import { FC, useCallback } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type Props = {
   /** 画像リスト */
@@ -18,50 +19,79 @@ type Props = {
 export const Carousel: FC<Props> = ({ images }: Props) => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
+  /** 選択中のインデックス */
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   /** 進むボタン押下時処理 */
-  const handleClickPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev();
-  }, [emblaApi]);
+  const handleClickPrev = () => {
+    emblaApi?.scrollNext();
+  };
 
   /** 戻るボタン押下時処理 */
-  const handleClickNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext();
-  }, [emblaApi]);
+  const handleClickNext = () => {
+    emblaApi?.scrollPrev();
+  };
+
+  /** ⚪︎ボタン押下時処理 */
+  const handleClickSelect = (index: number) => {
+    emblaApi?.scrollTo(index);
+  };
+
+  useEffect(() => {
+    emblaApi?.on('select', () => {
+      setSelectedIndex(emblaApi.selectedScrollSnap());
+    });
+  });
 
   return (
-    <div className="relative overflow-hidden" ref={emblaRef}>
-      <ul className="flex gap-x-4 pl-4">
-        {images.map((image, index) => (
-          <li
-            className="
+    <div className="flex flex-col gap-y-4">
+      <div className="relative overflow-hidden" ref={emblaRef}>
+        <ul className="flex gap-x-4 pl-4">
+          {images.map((image, index) => (
+            <li
+              className="
               flex aspect-[12/5] shrink-0 grow-0 basis-3/4 justify-center
             "
-            key={`carousel-image-list-${index}`}
-          >
-            <Image src={image.src} alt={image.alt} fill className="rounded-2xl" />
-          </li>
-        ))}
-      </ul>
-      <button
-        className="
+              key={`carousel-image-list-${index}`}
+            >
+              <Image src={image.src} alt={image.alt} fill className="rounded-2xl" />
+            </li>
+          ))}
+        </ul>
+        <button
+          className="
           absolute inset-y-0 left-[10%] my-auto h-12 w-12 rounded-full 
           bg-white font-bold text-slate-400 shadow duration-300
           hover:h-14 hover:w-14
         "
-        onClick={handleClickPrev}
-      >
-        ＜
-      </button>
-      <button
-        className="
+          onClick={handleClickPrev}
+        >
+          ＜
+        </button>
+        <button
+          className="
           absolute inset-y-0 right-[10%] my-auto h-12 w-12 rounded-full 
         bg-white font-bold text-slate-400 shadow duration-300
           hover:h-14 hover:w-14
         "
-        onClick={handleClickNext}
-      >
-        ＞
-      </button>
+          onClick={handleClickNext}
+        >
+          ＞
+        </button>
+      </div>
+      <ul className="flex justify-center gap-x-3">
+        {images.map((_, index) => (
+          <li className="h-3 w-3" key={`carousel-select-button-${index}`}>
+            <button
+              className={twMerge(
+                `h-full w-full rounded-full bg-slate-200`,
+                selectedIndex === index && `bg-slate-400`
+              )}
+              onClick={() => handleClickSelect(index)}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
