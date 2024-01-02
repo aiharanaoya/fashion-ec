007bf8ada587e7b92990c0019121211e14689760
aiharanaoya <nao11aihara@gmail.com>
2024-01-02 19:14:36 +0900

    ページ幅調整

--- app/(features)/page.tsx
+++ app/(features)/page.tsx
@@ -2,9 +2,9 @@ import { Carousel } from '@/_components/Carousel';
 import { H2 } from '@/_components/H2';
 import { ItemCard } from '@/_components/ItemCard';
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex flex-col gap-y-16 p-8">
+    <main className="flex flex-col items-center gap-y-16 py-8">
       <Carousel
         images={[
           {
@@ -21,51 +21,53 @@ export default function Home() {
           }
         ]}
       />
-      <section className="flex flex-col gap-y-4">
-        <H2 text="新着アイテム" />
-        <ul className="flex w-full gap-x-4">
-          <li className="w-full">
-            <ItemCard
-              image={{
-                src: '/dummy-image.png',
-                alt: ''
-              }}
-              name="アイテム名"
-              price={5000}
-            />
-          </li>
-          <li className="w-full">
-            <ItemCard
-              image={{
-                src: '/dummy-image.png',
-                alt: ''
-              }}
-              name="アイテム名"
-              price={5000}
-            />
-          </li>
-          <li className="w-full">
-            <ItemCard
-              image={{
-                src: '/dummy-image.png',
-                alt: ''
-              }}
-              name="アイテム名"
-              price={5000}
-            />
-          </li>
-          <li className="w-full">
-            <ItemCard
-              image={{
-                src: '/dummy-image.png',
-                alt: ''
-              }}
-              name="アイテム名"
-              price={5000}
-            />
-          </li>
-        </ul>
-      </section>
+      <div className="h-full w-full max-w-5xl">
+        <section className="flex flex-col gap-y-5">
+          <H2 text="新着アイテム" />
+          <ul className="flex w-full gap-x-4">
+            <li className="w-full">
+              <ItemCard
+                image={{
+                  src: '/dummy-image.png',
+                  alt: ''
+                }}
+                name="アイテム名"
+                price={5000}
+              />
+            </li>
+            <li className="w-full">
+              <ItemCard
+                image={{
+                  src: '/dummy-image.png',
+                  alt: ''
+                }}
+                name="アイテム名"
+                price={5000}
+              />
+            </li>
+            <li className="w-full">
+              <ItemCard
+                image={{
+                  src: '/dummy-image.png',
+                  alt: ''
+                }}
+                name="アイテム名"
+                price={5000}
+              />
+            </li>
+            <li className="w-full">
+              <ItemCard
+                image={{
+                  src: '/dummy-image.png',
+                  alt: ''
+                }}
+                name="アイテム名"
+                price={5000}
+              />
+            </li>
+          </ul>
+        </section>
+      </div>
     </main>
   );
 }
