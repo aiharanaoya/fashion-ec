0418d07b08846e237e80dcf1f61dd0ef6497c333
aiharanaoya <nao11aihara@gmail.com>
2024-02-18 00:22:29 +0900

    ヘッダーのレスポンシブ対応を追加

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -7,11 +7,14 @@ import { ChangeEvent, FC, useState } from 'react';
 import { FormSearch } from '@/_components/FormSearch';
 import { IconButton } from '@/_components/IconButton';
 import { Logo } from '@/_components/Logo';
+import { useResponsive } from '@/_hooks/useResponsive';
 
 /**
  * ヘッダー
  */
 export const Header: FC = () => {
+  const { isPc } = useResponsive();
+
   /** 検索ワード */
   const [searchValue, setSearchValue] = useState('');
 
@@ -35,12 +38,12 @@ export const Header: FC = () => {
     console.log('ユーザーアイコン押下時処理');
   };
 
-  return (
+  return isPc ? (
     <header
       className="
-        sticky top-0 z-10 flex h-full w-full
-        items-center justify-between
-        border-b border-slate-300 bg-white px-8 py-2
+        sticky top-0 z-10 flex h-full w-full items-center
+        justify-between border-b
+        border-slate-300 bg-white px-8 py-2
       "
     >
       <Link href="/" className="duration-300 hover:opacity-70">
@@ -62,6 +65,35 @@ export const Header: FC = () => {
             <UserIcon className="w-6" />
           </IconButton>
         </div>
+      </div>
+    </header>
+  ) : (
+    <header
+      className="
+        sticky top-0 z-10 flex h-full w-full flex-col items-center
+        justify-between gap-y-2
+        border-b border-slate-300 bg-white px-8 py-2
+      "
+    >
+      <div className="flex w-full items-center justify-between">
+        <Link href="/" className="duration-300 hover:opacity-70">
+          <Logo />
+        </Link>
+        <div className="flex items-center gap-x-1">
+          <IconButton onClick={handleClickCart}>
+            <ShoppingCartIcon className="w-6" />
+          </IconButton>
+          <IconButton onClick={handleClickUser}>
+            <UserIcon className="w-6" />
+          </IconButton>
+        </div>
+      </div>
+      <div className="w-full text-sm">
+        <FormSearch
+          value={searchValue}
+          onChangeForm={changeFormSearch}
+          onClickButton={handleClickSearch}
+        />
       </div>
     </header>
   );
