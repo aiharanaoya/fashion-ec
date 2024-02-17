3c8342f03e477719d1ff16ce705355806316b56e
aiharanaoya <nao11aihara@gmail.com>
2024-02-17 23:56:29 +0900

    js内でレスポンシブタイプを判別するhookを追加

---
+++ app/_hooks/useResponsive.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+type Hook = {
+  /** PC表示か */
+  isPc: boolean;
+};
+
+/**
+ * js内でレスポンシブタイプを判別するhook
+ */
+export const useResponsive = (): Hook => {
+  /** PC表示か */
+  const [isPc, setIsPc] = useState(false);
+
+  /** resize時の処理 */
+  const handleResize = () => {
+    setIsPc(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return {
+    isPc
+  };
+};
