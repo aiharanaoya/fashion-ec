cd5c477568203ce5d863dbab0462e09988d1c34b
aiharanaoya <nao11aihara@gmail.com>
2024-03-07 13:03:30 +0900

    アイテムセクションコンポーネント作成

---
+++ src/components/ItemSection/index.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+
+import { H2 } from '@/components/H2';
+import { ItemCard } from '@/components/ItemCard';
+
+type Props = {
+  /** セクション名 */
+  headlineText: string;
+  /** アイテムリスト */
+  itemList: {
+    /** 画像 */
+    image: {
+      src: string;
+      alt: string;
+    };
+    /** 名前 */
+    name: string;
+    /** 価格 */
+    price: number;
+  }[];
+};
+
+/**
+ * アイテムセクション
+ */
+export const ItemSection: FC<Props> = ({ headlineText, itemList }: Props) => {
+  return (
+    <section className="flex flex-col gap-y-4">
+      <H2 text={headlineText} />
+      <ul className="flex w-full gap-x-4">
+        {itemList.map((item, index) => (
+          <li className="w-full" key={`item-list-${index}`}>
+            <ItemCard
+              image={{
+                src: item.image.src,
+                alt: item.image.alt
+              }}
+              name={item.name}
+              price={item.price}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
