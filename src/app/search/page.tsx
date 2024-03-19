8ebfc8c37eb836fe20cc65f16670b1f4032e870f
aiharanaoya <nao11aihara@gmail.com>
2024-03-20 00:29:10 +0900

    検索結果のテキストを動的に取得するように修正

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 import { ItemSection } from '@/components/ItemSection';
 
 export default function Page() {
@@ -85,10 +89,14 @@ export default function Page() {
     }
   ];
 
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get('query');
+
   return (
     <main className="flex justify-center pb-16 pt-8">
       <div className="h-full w-full max-w-5xl px-4">
-        <ItemSection headlineText="〜〜の検索結果" itemList={dummyItemList} />
+        <ItemSection headlineText={`${query}の検索結果`} itemList={dummyItemList} />
       </div>
     </main>
   );
