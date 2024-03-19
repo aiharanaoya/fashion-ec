d89df6a08057e0f4c75417826d71b2c719dc02bb
aiharanaoya <nao11aihara@gmail.com>
2024-03-20 00:03:29 +0900

    検索一覧マークアップ完成

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -34,12 +34,60 @@ export default function Page() {
       },
       name: 'アイテム名アイテム名アイテム名',
       price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
+    },
+    {
+      image: {
+        src: '/dummy-image.png',
+        alt: ''
+      },
+      name: 'アイテム名アイテム名アイテム名',
+      price: 5000
     }
   ];
 
   return (
-    <main className="flex items-center px-4 pb-16 pt-8">
-      <div className="h-full w-full max-w-5xl">
+    <main className="flex justify-center pb-16 pt-8">
+      <div className="h-full w-full max-w-5xl px-4">
         <ItemSection headlineText="〜〜の検索結果" itemList={dummyItemList} />
       </div>
     </main>
