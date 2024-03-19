d7d9ab8d44fb5b8a0777ea7a6364e05447e2ad90
aiharanaoya <nao11aihara@gmail.com>
2024-03-20 00:40:31 +0900

    useSearchParamsを使う箇所をコンポーネント化する

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { FC, Suspense } from 'react';
 
 import { ItemSection } from '@/components/ItemSection';
 
@@ -90,16 +90,23 @@ export default function Page() {
     }
   ];
 
-  const searchParams = useSearchParams();
+  /** 検索結果セクション */
+  const ResultSection: FC = () => {
+    const searchParams = useSearchParams();
+
+    return (
+      <ItemSection
+        headlineText={`${searchParams.get('query')}の検索結果`}
+        itemList={dummyItemList}
+      />
+    );
+  };
 
   return (
     <main className="flex justify-center py-16 pt-8">
       <div className="h-full w-full max-w-5xl px-4">
         <Suspense fallback={<p>loading</p>}>
-          <ItemSection
-            headlineText={`${searchParams.get('query')}の検索結果`}
-            itemList={dummyItemList}
-          />
+          <ResultSection />
         </Suspense>
       </div>
     </main>
