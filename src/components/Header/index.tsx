685a91a073db318a1e26018db1ec1ee2c30c8c36
aiharanaoya <nao11aihara@gmail.com>
2024-03-22 23:35:09 +0900

    ヘッダーでuseResponsiveを使わないように修正

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,14 +7,11 @@ import { ChangeEvent, FC, useState } from 'react';
 import { FormSearch } from '@/components/FormSearch';
 import { IconButton } from '@/components/IconButton';
 import { Logo } from '@/components/Logo';
-import { useResponsive } from '@/hooks/useResponsive';
 
 /**
  * ヘッダー
  */
 export const Header: FC = () => {
-  const { isPc } = useResponsive();
-
   /** 検索ワード */
   const [searchValue, setSearchValue] = useState('');
 
@@ -33,63 +30,64 @@ export const Header: FC = () => {
     console.log('ユーザーアイコン押下時処理');
   };
 
-  return isPc ? (
-    <header
-      className="
-        sticky top-0 z-10 flex h-full w-full items-center
+  return (
+    <>
+      <header
+        className="
+        sticky top-0 z-10 hidden h-full w-full items-center
         justify-between border-b
-        border-slate-300 bg-white px-8 py-2
+        border-slate-300 bg-white px-8 py-2 md:flex
       "
-    >
-      <Link href="/" className="duration-300 hover:opacity-70">
-        <Logo />
-      </Link>
-      <div className="flex items-center gap-x-2">
-        <div className="w-96 text-sm">
+      >
+        <Link href="/" className="duration-300 hover:opacity-70">
+          <Logo />
+        </Link>
+        <div className="flex items-center gap-x-2">
+          <div className="w-96 text-sm">
+            <FormSearch
+              href={`/search?query=${searchValue}`}
+              value={searchValue}
+              onChangeForm={changeFormSearch}
+            />
+          </div>
+          <div className="flex items-center gap-x-1">
+            <IconButton onClick={handleClickCart}>
+              <ShoppingCartIcon className="w-6" />
+            </IconButton>
+            <IconButton onClick={handleClickUser}>
+              <UserIcon className="w-6" />
+            </IconButton>
+          </div>
+        </div>
+      </header>
+      <header
+        className="
+        sticky top-0 z-10 flex h-full w-full flex-col items-center
+        justify-between gap-y-2
+        border-b border-slate-300 bg-white px-8 py-2 md:hidden
+      "
+      >
+        <div className="flex w-full items-center justify-between">
+          <Link href="/" className="duration-300 hover:opacity-70">
+            <Logo />
+          </Link>
+          <div className="flex items-center gap-x-1">
+            <IconButton onClick={handleClickCart}>
+              <ShoppingCartIcon className="w-6" />
+            </IconButton>
+            <IconButton onClick={handleClickUser}>
+              <UserIcon className="w-6" />
+            </IconButton>
+          </div>
+        </div>
+        <div className="w-full text-sm">
           <FormSearch
             href={`/search?query=${searchValue}`}
             value={searchValue}
             onChangeForm={changeFormSearch}
           />
         </div>
-        <div className="flex items-center gap-x-1">
-          <IconButton onClick={handleClickCart}>
-            <ShoppingCartIcon className="w-6" />
-          </IconButton>
-          <IconButton onClick={handleClickUser}>
-            <UserIcon className="w-6" />
-          </IconButton>
-        </div>
-      </div>
-    </header>
-  ) : (
-    <header
-      className="
-        sticky top-0 z-10 flex h-full w-full flex-col items-center
-        justify-between gap-y-2
-        border-b border-slate-300 bg-white px-8 py-2
-      "
-    >
-      <div className="flex w-full items-center justify-between">
-        <Link href="/" className="duration-300 hover:opacity-70">
-          <Logo />
-        </Link>
-        <div className="flex items-center gap-x-1">
-          <IconButton onClick={handleClickCart}>
-            <ShoppingCartIcon className="w-6" />
-          </IconButton>
-          <IconButton onClick={handleClickUser}>
-            <UserIcon className="w-6" />
-          </IconButton>
-        </div>
-      </div>
-      <div className="w-full text-sm">
-        <FormSearch
-          href={`/search?query=${searchValue}`}
-          value={searchValue}
-          onChangeForm={changeFormSearch}
-        />
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
