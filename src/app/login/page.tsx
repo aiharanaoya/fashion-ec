0957db4fad043ce8f0edc4897096d80628ac5d62
aiharanaoya <nao11aihara@gmail.com>
2024-03-24 12:26:31 +0900

    メールアドレス、パスワードの入力処理追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
 
 import { Button } from '@/components/Button';
 import { H2 } from '@/components/H2';
@@ -9,6 +10,22 @@ import { InputText } from '@/components/InputText';
 export default function Page() {
   const router = useRouter();
 
+  /** メールアドレス入力値 */
+  const [mailAddressValue, setMailAddressValue] = useState('');
+
+  /** パスワード入力値 */
+  const [passwordValue, setPasswordValue] = useState('');
+
+  /** メールアドレス入力処理 */
+  const changeMailAddressValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setMailAddressValue(e.target.value);
+  };
+
+  /** パスワード入力処理 */
+  const changePasswordValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordValue(e.target.value);
+  };
+
   return (
     <div className="flex h-full w-full flex-col items-center gap-y-12 px-4 py-12">
       <form className="flex h-full w-full max-w-md flex-col gap-y-8 rounded-2xl bg-slate-100 p-12">
@@ -16,11 +33,11 @@ export default function Page() {
         <div className="flex flex-col gap-y-4">
           <div className="flex flex-col gap-y-1">
             <p className="text-sm">メールアドレス</p>
-            <InputText value={0} onChange={() => {}} />
+            <InputText value={mailAddressValue} onChange={changeMailAddressValue} type="email" />
           </div>
           <div className="flex flex-col gap-y-1">
             <p className="text-sm">パスワード</p>
-            <InputText value={0} onChange={() => {}} />
+            <InputText value={passwordValue} onChange={changePasswordValue} type="password" />
           </div>
         </div>
         <Button
