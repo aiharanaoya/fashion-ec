ed90c721f9ce253ba857e5a4ecebfa65e56b5406
aiharanaoya <nao11aihara@gmail.com>
2024-04-03 23:04:24 +0900

    padding調整

--- src/components/PageCard/index.tsx
+++ src/components/PageCard/index.tsx
@@ -10,7 +10,7 @@ type Props = {
  */
 export const PageCard: FC<Props> = ({ children }: Props) => {
   return (
-    <div className="h-full w-full max-w-xl rounded-2xl bg-slate-100 p-10 shadow-lg md:p-14">
+    <div className="h-full w-full max-w-xl rounded-2xl bg-slate-100 p-10 shadow-lg md:p-16">
       {children}
     </div>
   );
