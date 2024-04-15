bfff27129a46000eacb1fb28e8b9fe813d6e0735
aiharanaoya <nao11aihara@gmail.com>
2024-04-16 00:12:28 +0900

    CartItem一旦完成

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FC } from 'react';
 
+import { Button } from '@/components/Button';
 import { ListItem } from '@/types/item';
 
 type Props = Omit<ListItem, 'id'> & {
@@ -14,12 +15,17 @@ type Props = Omit<ListItem, 'id'> & {
 export const CartItem: FC<Props> = ({ image, name, price, onClickDelete }: Props) => {
   return (
     <div className="flex gap-x-4">
-      <div className="relative aspect-[9/10] h-full w-full">
+      <div className="relative aspect-[9/10] h-full w-full flex-1">
         <Image className="rounded-2xl object-cover" src={image.src} alt={image.alt} fill />
       </div>
-      <div className="flex flex-col gap-y-2">
-        <p className="text-sm">{name}</p>
-        <p className="text-sm">¥{price}</p>
+      <div className="flex flex-1 flex-col justify-center gap-y-4 py-4">
+        <div className="flex flex-col gap-y-2">
+          <p>{name}</p>
+          <p>¥{price}</p>
+        </div>
+        <div>
+          <Button onClick={onClickDelete}>削除</Button>
+        </div>
       </div>
     </div>
   );
