5a9e377018af128ca48a37cefdc755db0679c1aa
aiharanaoya <nao11aihara@gmail.com>
2024-04-18 22:47:45 +0900

    デフォルトスタイルにlist-noneを追加

--- src/styles/defaultStyle.ts
+++ src/styles/defaultStyle.ts
@@ -3,4 +3,12 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 /** デフォルトスタイル */
-export const defaultStyle = `${inter.className} text-slate-800 antialiased`;
+export const defaultStyle = `
+    ${inter.className}
+    text-slate-800
+    antialiased
+    list-none
+    flex
+    min-h-screen
+    flex-col
+`;
