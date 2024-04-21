7fc57dac638750dc8d14d4476099a88536aba7a2
aiharanaoya <nao11aihara@gmail.com>
2024-04-22 00:25:12 +0900

    カルーセルを画面側に移動

--- src/app/(contents)/page.tsx
+++ src/app/(contents)/page.tsx
@@ -1,14 +1,31 @@
 import { fetchItemList } from '@/actions/item';
 import { Carousel } from '@/components//Carousel';
 import { ItemSection } from '@/components/ItemSection';
-import { dummyCarouselImageList } from '@/constants/dummy';
+import { ImageItem } from '@/types/item';
 
 export default async function Page() {
+  /** カルーセル画像リスト */
+  const carouselImageList: Array<ImageItem> = [
+    {
+      src: '/carousel-image-1.webp',
+      alt: ''
+    },
+    {
+      src: '/carousel-image-2.webp',
+      alt: ''
+    },
+    {
+      src: '/carousel-image-3.webp',
+      alt: ''
+    }
+  ];
+
+  /** アイテム一覧取得 */
   const itemList = await fetchItemList();
 
   return (
     <div className="flex flex-col items-center gap-y-16 pb-16 pt-8">
-      <Carousel images={dummyCarouselImageList} />
+      <Carousel images={carouselImageList} />
       <div className="flex h-full w-full max-w-5xl flex-col gap-y-16 px-4">
         <ItemSection headlineText="新着アイテム" itemList={itemList} />
         <ItemSection headlineText="おすすめアイテム" itemList={itemList} />
