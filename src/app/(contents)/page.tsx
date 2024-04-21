7bc3b8f390ef3ac82c89f452d88972bde12923af
aiharanaoya <nao11aihara@gmail.com>
2024-04-22 00:14:32 +0900

    fetchItemListをページに組み込み

--- src/app/(contents)/page.tsx
+++ src/app/(contents)/page.tsx
@@ -1,17 +1,17 @@
+import { fetchItemList } from '@/actions/item';
 import { Carousel } from '@/components//Carousel';
 import { ItemSection } from '@/components/ItemSection';
-import { dummyCarouselImageList, dummyListItem } from '@/constants/dummy';
+import { dummyCarouselImageList } from '@/constants/dummy';
 
-export default function Page() {
-  /** ダミーアイテムリスト */
-  const dummyItemList = new Array(4).fill(dummyListItem);
+export default async function Page() {
+  const itemList = await fetchItemList();
 
   return (
     <div className="flex flex-col items-center gap-y-16 pb-16 pt-8">
       <Carousel images={dummyCarouselImageList} />
       <div className="flex h-full w-full max-w-5xl flex-col gap-y-16 px-4">
-        <ItemSection headlineText="新着アイテム" itemList={dummyItemList} />
-        <ItemSection headlineText="おすすめアイテム" itemList={dummyItemList} />
+        <ItemSection headlineText="新着アイテム" itemList={itemList} />
+        <ItemSection headlineText="おすすめアイテム" itemList={itemList} />
       </div>
     </div>
   );
