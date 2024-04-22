c44664116e4a9677fe561b4d329165233af5b479
aiharanaoya <nao11aihara@gmail.com>
2024-04-23 01:47:12 +0900

    seedデータを更新

--- src/db/data.js
+++ src/db/data.js
@@ -1,37 +1,37 @@
 const ItemData = [
   {
-    name: 'ブルーシャツ1',
+    name: 'シャツ ブルー',
     price: 5000,
-    description: '上品なブルーシャツです',
+    description: '上品なシャツです',
     image: {
       src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/blue-shirt-t7CJpJnGQ36vzpIdSXQvFwuhotTeRc.webp',
       alt: ''
     }
   },
   {
-    name: 'ブルーシャツ2',
+    name: 'シャツ ネイビー',
     price: 5000,
-    description: '上品なブルーシャツです',
+    description: '上品なシャツです',
     image: {
-      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/blue-shirt-t7CJpJnGQ36vzpIdSXQvFwuhotTeRc.webp',
+      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/navy-shirt-An5Z0z5TckDgoLYz2TpDHjRdfkKhwt.webp',
       alt: ''
     }
   },
   {
-    name: 'ブルーシャツ3',
-    price: 5000,
-    description: '上品なブルーシャツです',
+    name: 'トレンチコート ベージュ',
+    price: 15000,
+    description: '上品なトレンチコートです',
     image: {
-      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/blue-shirt-t7CJpJnGQ36vzpIdSXQvFwuhotTeRc.webp',
+      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/trench-coat-beige-wcFGUocmLpFpuB9SwTDBxXP0I62xpt.webp',
       alt: ''
     }
   },
   {
-    name: 'ブルーシャツ4',
-    price: 5000,
-    description: '上品なブルーシャツです',
+    name: 'トレンチコート ダークグレー',
+    price: 15000,
+    description: '上品なトレンチコートです',
     image: {
-      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/blue-shirt-t7CJpJnGQ36vzpIdSXQvFwuhotTeRc.webp',
+      src: 'https://ge71tzot7qbveuk9.public.blob.vercel-storage.com/trench-coat-dark-gray-psVyTkdhmkwLhAAN0knkftVlGworUY.webp',
       alt: ''
     }
   }
