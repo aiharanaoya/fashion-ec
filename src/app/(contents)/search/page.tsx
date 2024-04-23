de51bf1233a842559436ac928faad5b0c98993d2
aiharanaoya <nao11aihara@gmail.com>
2024-04-24 01:20:44 +0900

    useSearchParamsではなくsearchParamsを使うように修正

--- src/app/(contents)/search/page.tsx
+++ src/app/(contents)/search/page.tsx
@@ -1,34 +1,21 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
-import { FC, Suspense } from 'react';
-
 import { ItemSection } from '@/components/ItemSection';
 import { dummyListItem } from '@/constants/dummy';
 
-export default function Page() {
+export default function Page({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  /** 検索クエリ */
+  const query = searchParams['query'];
+
   /** ダミーアイテムリスト */
   const dummyItemList = new Array(10).fill(dummyListItem);
-
-  /** 検索結果セクション */
-  const ResultSection: FC = () => {
-    const searchParams = useSearchParams();
-
-    return (
-      <ItemSection
-        headlineText={`${searchParams.get('query')}の検索結果`}
-        itemList={dummyItemList}
-      />
-    );
-  };
 
   return (
     <div className="flex justify-center py-16 pt-8">
       <div className="h-full w-full max-w-5xl px-4">
-        {/* TODO: loading部分はスケルトン作る */}
-        <Suspense fallback={<p>loading</p>}>
-          <ResultSection />
-        </Suspense>
+        <ItemSection headlineText={`${query}の検索結果`} itemList={dummyItemList} />
       </div>
     </div>
   );
