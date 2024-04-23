97d4f7806926b1cccf874883d8276523ccd6c22e
aiharanaoya <nao11aihara@gmail.com>
2024-04-24 01:50:11 +0900

    検索画面に適用

--- src/app/(contents)/search/page.tsx
+++ src/app/(contents)/search/page.tsx
@@ -1,23 +1,23 @@
+import { fetchItemList } from '@/actions/item';
 import { ItemSection } from '@/components/ItemSection';
-import { dummyListItem } from '@/constants/dummy';
 
-export default function Page({
+export default async function Page({
   searchParams
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   /** 検索クエリ */
-  const query = searchParams['query'];
+  const query = searchParams['query'] as string;
 
-  /** ダミーアイテムリスト */
-  const dummyItemList = new Array(10).fill(dummyListItem);
+  /** アイテム一覧取得 */
+  const itemList = await fetchItemList(query);
 
   return (
     <div className="flex justify-center py-16 pt-8">
       <div className="h-full w-full max-w-5xl px-4">
         <ItemSection
           headlineText={query ? `${query}の検索結果` : '全てのアイテム'}
-          itemList={dummyItemList}
+          itemList={itemList}
         />
       </div>
     </div>
