fe0e00ccc4a02d22095ccbd2b7c3cbbfdefcb945
aiharanaoya <nao11aihara@gmail.com>
2024-05-14 23:59:02 +0900

    email、password直打ちの場合

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,25 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: []
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {}
+      },
+      authorize: async (credentials) => {
+        const matched = credentials?.email === 'email' && credentials?.password === 'password';
+
+        if (!matched) {
+          return null;
+        }
+
+        return {
+          email: 'email',
+          password: 'password'
+        };
+      }
+    })
+  ]
 });
