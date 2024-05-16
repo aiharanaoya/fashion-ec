7c55b0717411ba081ba13d801fd271529b308f34
aiharanaoya <nao11aihara@gmail.com>
2024-05-17 00:52:59 +0900

    ログインスキーマ作成

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,8 +1,21 @@
 import { compare } from 'bcrypt';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import { object, string } from 'zod';
 
 import { fetchUserByEmail } from '@/actions/user';
+
+/** ログインスキーマ */
+const loginSchema = object({
+  email: string({ required_error: 'メールアドレスは必須です' })
+    .min(1, 'メールアドレスは必須です')
+    .max(100, 'メールアドレスは100文字以下で入力してください')
+    .email('メールアドレスの形式にしてください'),
+  password: string({ required_error: 'パスワードは必須です' })
+    .min(1, 'パスワードは必須です')
+    .min(8, 'パスワードは8文字以上で入力してください')
+    .max(100, 'パスワードは100文字以下で入力してください')
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -12,7 +25,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {}
       },
       authorize: async (credentials) => {
-        const { email, password } = credentials;
+        const parsedCredentials = loginSchema.safeParse(credentials);
+
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
 
         const user = await fetchUserByEmail(email as string);
 
@@ -25,8 +44,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         if (!isMatch) {
           return null;
         }
-
-        console.log('ログインに成功しました');
 
         return user;
       }
